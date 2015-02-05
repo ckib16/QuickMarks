7f8f2dd4e729430dcda2687669c5e99e30df98ad
ckib16 <ckib16@gmail.com>
2015-02-04 21:53:58 -0500

    US#4 (Topics CRUD) complete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   def index
     @topics = Topic.all
+    @topic = Topic.new
   end
 
   def show
@@ -8,8 +9,46 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(params.require(:topic).permit(:title))
+    if @topic.save
+      flash[:notice] = "Topic was successfully created..."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error saving your topic..."
+      render :new
+    end
   end
 
   def edit
+    @topic = Topic.find(params[:id])
   end
+
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update_attributes(params.require(:topic).permit(:title))
+      flash[:notice] = "Topic was successfully updated..."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error updating your topic..."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    title = @topic.title
+
+    if @topic.destroy
+      flash[:notice] = "\"#{title}\" was deleted successfully..."
+      redirect_to topics_path
+    else
+      flash[:error] = "Ther was an error deleting the topic"
+      render :show
+    end
+  end
+
 end
